06866f2cea83a37740321dd8527ee754ceb80a5d
shlok-bit <80306779+shlok-bit@users.noreply.github.com>
2021-03-27 20:29:57 +0530

    Add files via upload

---
+++ python spiral.py
@@ -0,0 +1,14 @@
+import turtle
+
+turtle.bgcolor("black")
+turtle.pensize(2)
+turtle.speed(0)
+
+
+for i in range(450):
+    for colours in ["red", "magenta", "blue", "cyan", "green", "yellow", "white"]:
+        turtle.color(colours)
+        turtle.circle(100)
+        turtle.left(10)
+
+turtle.hideturtle
